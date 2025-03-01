316768f46e6b3da2c0f6df2f160178244c69a560
AnushaMotamarri <motamarrianusha01@gmail.com>
2025-03-01 15:42:14 +0530

    showed the list page

--- src/components/body.js
+++ src/components/body.js
@@ -1,13 +1,18 @@
 import React,{useState,useEffect} from 'react';
 import Shimmer from './shimmer';
+import StarSvg from './svgs/star_svg';
 
 const RestaurantCard = ({restaurantData}) =>{
     return (
         <div className="card">
-            <img src = {`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${restaurantData?.data?.cloudinaryImageId}`}></img>
-            <h2>{restaurantData?.data?.name}</h2>
-            <h3>{restaurantData?.data?.cuisines?.join(' ,')}</h3>
-            <h4>{restaurantData?.data?.avgRating}</h4>
+            <div className='image-container'>
+                <img src = {`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${restaurantData?.info?.cloudinaryImageId}`}></img>
+
+            </div>
+                <div className='title'>{restaurantData?.info?.name}</div>
+                <div className='rating'><StarSvg/>{restaurantData?.info?.avgRating}</div>
+                <div className='cusines'>{restaurantData?.info?.cuisines?.join(', ')}</div>
+                <div className='locality'>{restaurantData?.info?.locality}</div>
         </div>
     )
 }
@@ -17,12 +22,11 @@ export default Body=()=>{
     const [filteredRestaurantList,setFilteredRestaurantList]  = useState([])
     async function getRestaurants(){
         try{
-            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.3730192&lng=78.547636&page_type=DESKTOP_WEB_LISTING");
+            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.956924&lng=77.701127&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
             const json = await data.json();
-            const cardsData = json?.data?.cards[2]?.data?.data?.cards||[];
+            const cardsData = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants||[];
             setRestaurantList(cardsData);
             setFilteredRestaurantList(cardsData)
-       
         }
         catch(e){
             console.log(e)
@@ -34,7 +38,6 @@ export default Body=()=>{
     },[]);
     function filterRestaurants(){
         const filteredCardsData = restaurantList.filter(rest=>((rest.data.name).toLowerCase()).includes(searchText.toLowerCase()));
-        console.log(restaurantList,searchText,filteredCardsData)
         setFilteredRestaurantList(filteredCardsData)
     }
     return filteredRestaurantList.length===0?<Shimmer/>:<>
